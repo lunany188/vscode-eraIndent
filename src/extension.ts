e5c3501699794dfa2c962637bee811c12585606a
lunany188 <lunany188@gmail.com>
2019-01-18 13:43:25 +0900

    範囲と入力時に多少対応

--- src/extension.ts
+++ src/extension.ts
@@ -3,46 +3,64 @@
 import * as vscode from 'vscode';
 import * as indentor from './indenter';
 
-export class EraIndentProvider implements vscode.DocumentFormattingEditProvider {
-	private diagonostics: vscode.DiagnosticCollection;
-	constructor(diagnostics: vscode.DiagnosticCollection) {
-		this.diagonostics = diagnostics;
-	}
-	public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-		const hoge = new indentor.EraIndenter(options);
-		const ret: vscode.TextEdit[] = [];
-		this.diagonostics.clear();
-		for (let lineNo = 0; lineNo < document.lineCount; lineNo++) {
-			const line: vscode.TextLine = document.lineAt(lineNo);
-			const result = hoge.update(line);
-			// 仮 エラーが起きても次の行を読めるようにうまいことやる
-			if (indentor.isEraIndenterError(result)) {
-				const test = this.diagonostics.get(document.uri);
-				const newDiagnostic = new vscode.Diagnostic(document.lineAt(result.lineNumber).range, result.message);
-				if (test===undefined) {
-					this.diagonostics.set(document.uri, [newDiagnostic]);
-				}
-				else {
-					test.push(newDiagnostic);
-					this.diagonostics.set(document.uri, test);
-				}
-				continue;
-			}
-			result.forEach(ns => ret.push(vscode.TextEdit.replace(document.lineAt(ns.lineNumber).range, ns.text)));
-		}
-		return ret;
-	}
+export class EraIndentProvider implements vscode.DocumentFormattingEditProvider, vscode.DocumentRangeFormattingEditProvider, vscode.OnTypeFormattingEditProvider{
+    private diagonostics: vscode.DiagnosticCollection;
+    constructor(diagnostics: vscode.DiagnosticCollection) {
+        this.diagonostics = diagnostics;
+    }
+    public provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+        const indenter = new indentor.EraIndenter(options);
+        return this.innnerFormat(document, document.lineAt(document.lineCount - 1).range.end.line, indenter);
+    }
+    provideDocumentRangeFormattingEdits(document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+        const indenter = new indentor.EraIndenter(options);
+        const endline = range.end.line;
+        return this.innnerFormat(document, endline, indenter);
+    }
+    provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, ch: string, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]>{
+        const indenter = new indentor.EraIndenter(options);
+        const result = this.innnerFormat(document, position.line, indenter);
+        return result;
+    }
+
+    private innnerFormat(doc: vscode.TextDocument, endline: number, indenter: indentor.EraIndenter): vscode.TextEdit[] {
+        const ret: vscode.TextEdit[] = [];
+        const diags = this.diagonostics.get(doc.uri);
+        let nextDiags: vscode.Diagnostic[];
+        if (diags !== undefined) {
+            nextDiags = diags.filter(value => value.range.end.line > endline);
+        }
+        else {
+            nextDiags = [];
+        }
+        for (let lineNo = 0; lineNo < endline; lineNo++) {
+            const line: vscode.TextLine = doc.lineAt(lineNo);
+            const result = indenter.update(line);
+            // 仮 エラーが起きても次の行を読めるようにうまいことやる
+            if (indentor.isEraIndenterError(result)) {
+                const newDiagnostic = new vscode.Diagnostic(doc.lineAt(result.lineNumber).range, result.message);
+                nextDiags.push(newDiagnostic);
+                continue;
+            }
+            result.forEach(ns => ret.push(vscode.TextEdit.replace(doc.lineAt(ns.lineNumber).range, ns.text)));
+        }
+        this.diagonostics.set(doc.uri, nextDiags);
+        return ret;
+    }
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const eraSelector = { language: 'erabasic', scheme: 'file' };
+    const diagnostics = vscode.languages.createDiagnosticCollection("erabasicIndenter");
+    context.subscriptions.push(diagnostics);
+    const provider = new EraIndentProvider(diagnostics);
 
-	const diagnostics = vscode.languages.createDiagnosticCollection("erabasicIndenter");
-	context.subscriptions.push(diagnostics);
-
-	const test = vscode.languages.registerDocumentFormattingEditProvider({ language: 'erabasic', scheme: 'file' } , new EraIndentProvider(diagnostics));
-	context.subscriptions.push(test);
+    const test = vscode.languages.registerDocumentRangeFormattingEditProvider(eraSelector, provider);
+    context.subscriptions.push(test);
+    const test2 = vscode.languages.registerOnTypeFormattingEditProvider(eraSelector, provider, "\n");
+    context.subscriptions.push(test2);
 }
 
 // this method is called when your extension is deactivated
